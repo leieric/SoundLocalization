e6671311fc466d122da8c021f5bb71d2b0926ba1
ltang500 <ltang500@gmail.com>
2018-05-17 13:51:20 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -13,10 +13,13 @@ unsigned int CORRLEN = 5999;
 
 void collectData(float32_t * vec0, float32_t * vec1); //retrieves data from both adc inputs
 void printUSB(float32_t* vec); //prints to USB cable
+void initLED(); //initialize LED pins to GPIO
+void lightLED(float32_t angle); //lights appropriate LEDs
 
 int main(){
 	hardware_init();
 	initADC();
+	initLED();
 	//init data arrays
 	float32_t *adc0Data = (float32_t*) malloc(VECLEN*sizeof(float32_t));
 	if (!adc0Data) {
@@ -41,10 +44,6 @@ int main(){
 	float32_t max; //correlation max
 	uint32_t maxIdx; //index of max
 	
-  //q31_t dummy1[3] = {2, 2, 2};
-	//q31_t dummy2[3] = {1, 1, 1};
-	//q31_t corr[5] = {0};
-	
 	int i;
 	float32_t timeDelay;
 	float32_t Ts = 1.7644E-6f;
@@ -59,8 +58,8 @@ int main(){
 		//debug_printf("Done.\r\nCalculating time delay...\r\n");
 		arm_max_f32(corrData, CORRLEN, &max, &maxIdx);
 		
-	/*int i;
-	for (i = 0; i < VECLEN; i++)
+	int i;
+	/*for (i = 0; i < VECLEN; i++)
 	{
 		debug_printf("%d %d\r\n", ((unsigned)adc0Data[i]), ((unsigned) adc1Data[i]));
 		//debug_printf("%d\r\n", (unsigned)[i]);
@@ -68,29 +67,44 @@ int main(){
 
 		debug_printf("%d %d\r\n", (unsigned)max, (unsigned)maxIdx);
 		timeDelay = Ts*(maxIdx - VECLEN);
-		angle = asin(343*timeDelay/dist)*180/3.14159265;
+		debug_printf("td: %d \r\n", (unsigned)timeDelay);
+		angle = asinf(343*timeDelay/dist)*180/3.14159265;
 		debug_printf("angle: %d \r\n", (unsigned)angle);
+		debug_printf("anglearg: %d \r\n", (unsigned)(100*343*timeDelay/dist));
+		PRINTF("%f\r\n", angle);
+		lightLED(angle);
 	}
 }
 
 void initLED() {
-	PORTC->PCR[5] = PORT_PCR_MUX(001);
-	PTC->PDDR = GPIO_PDDR_PDD(1<<5);
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK; //Enable Port C
+	PORTC->PCR[5] = PORT_PCR_MUX(001);  //Set pin as GPIO
+	PTC->PDDR |= (1 << 5); 							//Set as output	
 	PORTC->PCR[7] = PORT_PCR_MUX(001);
-	PTC->PDDR = GPIO_PDDR_PDD(1<<7);
+	PTC->PDDR |= (1<<7);
 	PORTC->PCR[0] = PORT_PCR_MUX(001);
-	PTC->PDDR = GPIO_PDDR_PDD(1<<0);
+	PTC->PDDR |= (1<<0);
 	PORTC->PCR[9] = PORT_PCR_MUX(001);
-	PTC->PDDR = GPIO_PDDR_PDD(1<<9);
+	PTC->PDDR |= (1<<9);
 	PORTC->PCR[8] = PORT_PCR_MUX(001);
-	PTC->PDDR = GPIO_PDDR_PDD(1<<8);
+	PTC->PDDR |= (1<<8);
+	PTC->PTOR |= 0x00003A1; //Flash all LEDs on
+	DelayFunction();
+	PTC->PTOR |= 0x00003A1;
 }
 
-void lightLED(float angle) {
-	if(angle < 36)
-		
-	
-	
+void lightLED(float32_t angle) {
+	PTC->PCOR |= 0xFFFFFFFF; //turn off all LEDs
+	if(angle < -54)
+		PTC->PSOR = (1 << 8);
+	else if(angle < -18)
+		PTC->PSOR = (1 << 9);
+	else if(angle < 18)
+		PTC->PSOR = (1 << 0);
+	else if(angle < 54)
+		PTC->PSOR = (1 << 7);
+	else if (angle < 90)
+		PTC->PSOR = (1 << 5);	
 }
 
 void collectData(float32_t* vec0, float32_t * vec1)
@@ -103,15 +117,15 @@ void collectData(float32_t* vec0, float32_t * vec1)
 		ADC0_SC1A = 0 & ADC_SC1_ADCH_MASK; //Write to SC1A to start conversion
 		while(ADC0_SC2 & ADC_SC2_ADACT_MASK); //Conversion in progress
 		while(!(ADC0_SC1A & ADC_SC1_COCO_MASK)); //Wait until conversion complete
-		vec0[i] = ((float32_t)ADC0_RA); //save to buffer
+		vec0[i] = (((float32_t)ADC0_RA)/1000); //save to buffer
 
 		ADC1_SC1A = 0 & ADC_SC1_ADCH_MASK; //Write to SC1A to start conversion
 		while(ADC1_SC2 & ADC_SC2_ADACT_MASK); //Conversion in progress
 		while(!(ADC1_SC1A & ADC_SC1_COCO_MASK)); //Wait until conversion complete
-		vec1[i] = ((float32_t)ADC1_RA); //save to buffer
+		vec1[i] = (((float32_t)ADC1_RA)/1000); //save to buffer
 	}
-	arm_mean_f32(vec0, VECLEN, &vec0Mean);
-	arm_mean_f32(vec1, VECLEN, &vec1Mean);
+	//arm_mean_f32(vec0, VECLEN, &vec0Mean);
+	//arm_mean_f32(vec1, VECLEN, &vec1Mean);
 	//arm_offset_f32(vec0, -vec0Mean, vec0, VECLEN);
 	//arm_offset_f32(vec1, -vec1Mean, vec1, VECLEN);
 }
